be241e2b0b9dfef443d933a53a49776166ad96cf
Zainab Azeem <z.azeem3105@gmail.com>
2026-04-08 05:10:00 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+  var button = document.getElementByID('entrybutton');
 
-
-
-
-
+function alert(){
+  var text = document.getElementByID('entryinput');
+  
+  
+  
+}
 
 
 window.addEventListener('load', init);
